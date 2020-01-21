4c8b86c9c9406876a16e4a40141e08e5590c0c70
Heri Susanto <herisusanto@mhcasia.com>
2020-01-21 23:23:34 +0700

    Revise login end poin

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 import sys
 import logging
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
-from resources.user import UserRegister, User
+# from security import authenticate, identity
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -17,13 +17,14 @@ app.logger.setLevel(logging.ERROR)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'susanto'
 api = Api(app)
 
-app.config['JWT_AUTH_URL_RULE'] = '/login'
+# app.config['JWT_AUTH_URL_RULE'] = '/login'
 # config JWT to expire within half an hour
 # app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
@@ -31,6 +32,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db
--- resources/user.py
+++ resources/user.py
@@ -1,42 +1,45 @@
 import sqlite3
 from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import UserModel
 
 
-class UserRegister(Resource):
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help='This field cannot be blank.')
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help='This field cannot be blank.')
 
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
-    parser.add_argument('full_name',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
-    parser.add_argument('address',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
-    parser.add_argument('phone',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
-    parser.add_argument('email',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
-    parser.add_argument('is_driver',
-                        type=str,
-                        required=True,
-                        help='This field cannot be blank.')
+
+class UserRegister(Resource):
+    # _user_parser.add_argument('full_name',
+    #                           type=str,
+    #                           required=True,
+    #                           help='This field cannot be blank.')
+    # _user_parser.add_argument('address',
+    #                           type=str,
+    #                           required=True,
+    #                           help='This field cannot be blank.')
+    # _user_parser.add_argument('phone',
+    #                           type=str,
+    #                           required=True,
+    #                           help='This field cannot be blank.')
+    # _user_parser.add_argument('email',
+    #                           type=str,
+    #                           required=True,
+    #                           help='This field cannot be blank.')
+    # _user_parser.add_argument('is_driver',
+    #                           type=str,
+    #                           required=True,
+    #                           help='This field cannot be blank.')
 
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {'message': 'A user with that username already exists'}, 400
 
@@ -61,3 +64,24 @@ class User(Resource):
             return {'message': 'User not found.'}, 404
         user.delete_from_db()
         return {'message': 'User deleted.'}, 200
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        # _user_parser = reqparse.RequestParser()
+        data = _user_parser.parse_args()
+        print(data, 'DATA')
+        user = UserModel.find_by_username(data['username'])
+        # this is what the 'auntheticate()' funtion used to do
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                # 'refresh_token': refresh_token,
+                'username': user.username,
+                'role': user.is_driver
+            }
+        else:
+            return {'message': 'Invalid credentials'}, 401
